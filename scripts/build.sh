2a9d2eae32edee277f2e6692efc27be4ed5ce99b
Rui Costa <ruicosta042@gmail.com>
2021-05-30 21:43:49 +0100

    feat: commonjs compatibility

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
 rimraf dist
+rimraf cjs
 tsc --build tsconfig.build.json
+
+esbuild src/index.cjs --bundle --platform=node --outfile=dist/index.cjs
 esbuild src/index.ts --bundle --format=esm --outfile=dist/index.js
+
+# node12 compatibility
+mkdir cjs && cp dist/index.cjs cjs/index.js
