52edcbb78733cce26d9cab09399e597b6ed3a682
Rui Costa <ruicosta042@gmail.com>
2021-07-13 00:30:26 +0100

    feat: include esm/cjs on package

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ rimraf cjs
 tsc --build tsconfig.build.json
 
 esbuild src/index.cjs --bundle --platform=node --outfile=dist/index.cjs
-esbuild src/index.ts --bundle --format=esm --outfile=dist/index.js
+esbuild src/index.ts --bundle  --platform=node --format=esm --outfile=dist/index.mjs
 
 # node12 compatibility
 mkdir cjs && cp dist/index.cjs cjs/index.js
